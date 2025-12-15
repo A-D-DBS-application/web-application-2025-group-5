a889eceeca397803e86c87b98f711913956b57a8
victorien V <victorien.vermeersch@ugent.be>
2025-12-15 19:05:16 +0100

    error geven bij foute url

--- app/utils/mapbox_utils.py
+++ app/utils/mapbox_utils.py
@@ -4,14 +4,20 @@ MAPBOX_TOKEN = "pk.eyJ1IjoiYmFydGJhdHNsZWVyIiwiYSI6ImNtaW5rbG8yYTBwd2cza3Nib3NvM
 
 def geocode_address(address):
     """Convert address to (lng, lat) coordinates using Mapbox Geocoding API."""
-    
+
     url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{address}.json"
-    
     params = {
         "access_token": MAPBOX_TOKEN
     }
 
-    r = requests.get(url, params=params).json()
+    try:
+        response = requests.get(url, params=params, timeout=5)
+        response.raise_for_status()  # check of er geen foute URL of token ofzo gegeven werd 
+        r = response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Mapbox request failed: {e}")
+        return None
 
     if "features" not in r or len(r["features"]) == 0:
         return None
@@ -19,4 +25,4 @@ def geocode_address(address):
     coords = r["features"][0]["geometry"]["coordinates"]
     lng, lat = coords[0], coords[1]
 
-    return lng, lat 
+    return lng, lat
